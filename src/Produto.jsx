c3e99c33f4ba44e302bfad7769025dfbd711e9dd
DomSmithh <douglas.gon22222@hotmail.com>
2022-06-29 09:35:44 -0400

    Removendo codigos fontes

--- src/Produto.jsx
+++
@@ -1,9 +0,0 @@
-function Produto( {produtos} ){
-    return(
-        <div>
-            <p>{produtos.nome} {produtos.valor} {produtos.user} {produtos.projeto}</p>
-        </div>
-    )
-}
-
-export default Produto;
